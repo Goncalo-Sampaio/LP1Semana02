aee70da7708e353462dc9ce9cefca8e5f1d41c01
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-01 13:28:58 +0000

    Added calculation of volume and surface area and parse of inputs to floats, Cilindro project

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,11 +6,18 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
+            //Asks the user to input heigh and radius and parses them into float
             Console.Write("Please insert the cylinder's height: ");
-            string height = Console.ReadLine();
+            string heightInput = Console.ReadLine();
+            float height = float.Parse(heightInput);
 
             Console.Write("Please insert the cylinder's radius: ");
-            string radius = Console.ReadLine();
+            string radiusInput = Console.ReadLine();
+            float radius = float.Parse(radiusInput);
+
+            //Calculate volume and surface area
+            float volume = MathF.PI * MathF.Pow(radius, 2) * height;
+            float surface = 2 * MathF.PI * radius * (radius * height);
         }
     }
 }
