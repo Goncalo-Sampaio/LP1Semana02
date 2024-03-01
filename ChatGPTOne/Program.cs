43e0ac1ddc2b40df1fcddd1a4e3abdcdb4539765
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-01 17:09:07 +0000

    Added the base while loop and allow the user to quit, ChatGPTOne project

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -10,8 +10,27 @@ namespace ChatGPTOne
             string question, answer;
             bool exit = false;
 
-            Console.Write("What's your question? ");
-            question = Console.ReadLine();
+            while (exit == false)
+            {
+                //ask the user what's their question
+                Console.Write("What's your question? ");
+                question = Console.ReadLine();
+
+                //check if its a know question or if the user wants to quit
+                switch (question)
+                {
+                    case "EXIT":
+                        exit = true;
+                        answer = "Goodbye!";
+                        break;
+                    default:
+                        answer = "I don't understand your question...";
+                        break;
+                }
+
+                //showing the answer to the user
+                Console.WriteLine(answer);
+            }
         }
     }
 }
