8481826dfd6e38c75a9eaf488c0062664a7b0cab
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-01 16:20:04 +0000

    Showing the user the max and min value of each type on ValoresEspeciais project

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,31 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Showing the user the min and max value of each type
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(char.MaxValue);
+            Console.WriteLine(char.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
         }
     }
 }
