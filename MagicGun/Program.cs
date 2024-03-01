d6a5eb39602fc012bb2c4086089c6fb2d0729888
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-01 17:28:40 +0000

    Added question of number of shots and created a basic for loop in the MagicGun project

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -6,7 +6,13 @@ namespace MagicGun
     {
         static void Main(string[] args)
         {
+            Console.Write("Number of shots: ");
+            string input = Console.ReadLine();
+            int n = int.Parse(input);
 
+            for (int i = 1; i <= n; i ++) {
+                Console.WriteLine(n);
+            }
         }
     }
 }
